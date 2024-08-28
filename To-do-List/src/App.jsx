b0f1b3ef6cfc87b66fdebc3623ec1a3f96147539
Pratik Devkota <114279879+Pratik58D@users.noreply.github.com>
2024-08-28 09:20:25 +0545

    intial Commit

---
+++ To-do-List/src/App.jsx
@@ -0,0 +1,16 @@
+import Todo from "./components/Todo"
+
+function App() {
+
+  return (
+    <>
+    
+    <div className="bg-slate-700 grid py-4  min-h-screen ">
+      <Todo />
+    </div>
+    </>
+
+  )
+}
+
+export default App
---
+++ To-do-List/src/components/Todo.jsx
@@ -0,0 +1,91 @@
+import React,{useState,useRef,useEffect} from 'react'
+import icon from "../assets/todo_icon.png"
+import TodoList from './TodoList'
+
+const Todo = () => {
+
+    const [todoList , setTodoList] = useState(localStorage.getItem("todos")?
+    JSON.parse(localStorage.getItem("todos")) : []);
+    const inputRef = useRef();
+
+
+    const add = () => {
+        const inputText  = inputRef.current.value.trim();
+        // console.log(inputText)
+
+        if (inputText === ""){
+            return null;
+        }
+        
+        const newTodo = {
+            id:Date.now(), 
+            text : inputText,
+            isComplete :false,
+        }
+
+        setTodoList((prev)=> [...prev,newTodo]);
+        inputRef.current.value = "";
+    }
+
+
+    const deleteTodo = (id)=>{
+        setTodoList((prev)=>{
+           return  prev.filter((todo)=> todo.id !== id)
+        })
+    }
+
+    const toggle = (id)=>{
+        setTodoList((prevTodos)=>{
+            return prevTodos.map((todo)=>{
+                if(todo.id === id){
+                    return {...todo,isComplete:!todo.isComplete}
+
+                }
+
+                return todo;
+            })
+        })
+    }
+
+
+   useEffect(()=>{
+    // console.log(todoList)
+    localStorage.setItem("todos",JSON.stringify(todoList))
+   },[todoList]) 
+
+  return (
+    <div className='bg-blue-400 place-self-center w-11/12
+    max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+    {/* ----------------title ------------------- */}
+    <div className='flex items-center mt-7 gap-2'>
+        <img src={icon} alt=""  className='w-8' />
+        <h1 className='text-3xl font-semibold color'>TO-DO LIST</h1>
+
+    </div>
+
+    {/* -----------------input box----------------- */}
+    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        <input ref={inputRef} className='bg-transparent border-0 outline-none
+        flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'
+         type="text" placeholder='Add your Task...' />
+
+        <button onClick={add} className='border-none rounded-full bg-blue-950 
+        w-32 h-14 text-white text-lg font-medium cursor-pointer'>
+            ADD +</button>
+    </div>
+
+    {/* ----------todo list--------------- */}
+    <div>
+
+        {todoList.map((item,index)=>{
+            return <TodoList key={index} text ={item.text} id={item.id}
+             isComplete= {item.isComplete}  deleteTodo={deleteTodo} toggle= {toggle}/>
+        })}
+
+    </div>
+      
+    </div>
+  )
+}
+
+export default Todo

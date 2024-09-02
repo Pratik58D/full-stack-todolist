4d2fbfd6717ecb280e37a794a54efb44ff5d48ad
Pratik Devkota <114279879+Pratik58D@users.noreply.github.com>
2024-09-02 22:27:53 +0545

    commit for frontend changes

--- To-do-List/src/components/Todo.jsx
+++ To-do-List/src/components/Todo.jsx
@@ -1,91 +1,82 @@
-import React,{useState,useRef,useEffect} from 'react'
-import icon from "../assets/todo_icon.png"
-import TodoList from './TodoList'
+import React, { useState, useRef, useEffect } from 'react';
+import icon from "../assets/todo_icon.png";
+import TodoList from './TodoList';
+import axios from 'axios';
 
 const Todo = () => {
-
-    const [todoList , setTodoList] = useState(localStorage.getItem("todos")?
-    JSON.parse(localStorage.getItem("todos")) : []);
+    const [todoList, setTodoList] = useState([]);
     const inputRef = useRef();
 
+    useEffect(() => {
+        const fetchTodos = async () => {
+            try {
+                const response = await axios.get('http://localhost:5050/api/todo');
+                setTodoList(response.data);
+            } catch (error) {
+                console.error("There was an error fetching the todos!", error);
+            }
+        };
+        fetchTodos();
+    }, []);
 
-    const add = () => {
-        const inputText  = inputRef.current.value.trim();
-        // console.log(inputText)
-
-        if (inputText === ""){
+    const add = async () => {
+        const inputText = inputRef.current.value.trim();
+        if (inputText === "") {
             return null;
         }
-        
-        const newTodo = {
-            id:Date.now(), 
-            text : inputText,
-            isComplete :false,
+
+        const newTodo = { text: inputText };
+        try {
+            const response = await axios.post('http://localhost:5050/api/todo/create', newTodo);
+            setTodoList([...todoList, response.data]);
+            inputRef.current.value = "";
+        } catch (error) {
+            console.error("There was an error adding the todo!", error);
         }
+    };
 
-        setTodoList((prev)=> [...prev,newTodo]);
-        inputRef.current.value = "";
-    }
+    const deleteTodo = async (id) => {
+        try {
+            await axios.delete(`http://localhost:5050/api/todo/delete/${id}`);
+            setTodoList(prev => prev.filter(todo => todo._id !== id));
+        } catch (error) {
+            console.error("There was an error deleting the todo!", error);
+        }
+    };
 
+    const toggle = (id) => {
+        setTodoList(prevTodos => prevTodos.map(todo => {
+            if (todo._id === id) {
+                return { ...todo, isComplete: !todo.isComplete };
+            }
+            return todo;
+        }));
+    };
 
-    const deleteTodo = (id)=>{
-        setTodoList((prev)=>{
-           return  prev.filter((todo)=> todo.id !== id)
-        })
-    }
+    return (
+        <div className='bg-blue-400 place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+            <div className='flex items-center mt-7 gap-2'>
+                <img src={icon} alt="" className='w-8' />
+                <h1 className='text-3xl font-semibold color'>TO-DO LIST</h1>
+            </div>
 
-    const toggle = (id)=>{
-        setTodoList((prevTodos)=>{
-            return prevTodos.map((todo)=>{
-                if(todo.id === id){
-                    return {...todo,isComplete:!todo.isComplete}
+            <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+                <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'
+                    type="text" placeholder='Add your Task...' name='text' />
 
-                }
+                <button onClick={add} className='border-none rounded-full bg-blue-950 w-32 h-14 text-white text-lg font-medium cursor-pointer'>
+                    ADD +
+                </button>
+            </div>
 
-                return todo;
-            })
-        })
-    }
+            <div>
+                {todoList.map((item) => (
+                    <TodoList key={item._id} text={item.text} id={item._id}
+                        isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-
-   useEffect(()=>{
-    // console.log(todoList)
-    localStorage.setItem("todos",JSON.stringify(todoList))
-   },[todoList]) 
-
-  return (
-    <div className='bg-blue-400 place-self-center w-11/12
-    max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
-    {/* ----------------title ------------------- */}
-    <div className='flex items-center mt-7 gap-2'>
-        <img src={icon} alt=""  className='w-8' />
-        <h1 className='text-3xl font-semibold color'>TO-DO LIST</h1>
-
-    </div>
-
-    {/* -----------------input box----------------- */}
-    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input ref={inputRef} className='bg-transparent border-0 outline-none
-        flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'
-         type="text" placeholder='Add your Task...' />
-
-        <button onClick={add} className='border-none rounded-full bg-blue-950 
-        w-32 h-14 text-white text-lg font-medium cursor-pointer'>
-            ADD +</button>
-    </div>
-
-    {/* ----------todo list--------------- */}
-    <div>
-
-        {todoList.map((item,index)=>{
-            return <TodoList key={index} text ={item.text} id={item.id}
-             isComplete= {item.isComplete}  deleteTodo={deleteTodo} toggle= {toggle}/>
-        })}
-
-    </div>
-      
-    </div>
-  )
-}
-
-export default Todo
+export default Todo;
